5ce64cabecef687cefd6b04e63e40f0810d5ab9f
sio2 <antoninfouchet@gmail.com>
2025-12-15 13:20:10 +0100

    Ajout du mapper de Contrat

---
+++ src/main/java/bts/sio/azurimmo/model/mapper/ContratMapper.java
@@ -0,0 +1,35 @@
+package bts.sio.azurimmo.model.mapper;
+
+import bts.sio.azurimmo.model.Appartement;
+import bts.sio.azurimmo.model.Contrat;
+import bts.sio.azurimmo.model.dto.AppartementDTO;
+import bts.sio.azurimmo.model.dto.ContratDTO;
+
+public class ContratMapper {
+	
+	public static ContratDTO toDTO(Contrat c) {
+		 if (c == null) return null;
+		 
+		 	ContratDTO dto = new ContratDTO();
+	        dto.setLibelle(c.getLibelle()); 
+	        dto.setDate_debut(c.getDateDebut());
+	        dto.setDate_fin(c.getDateFin());
+	        dto.setMontant_brut(c.getMontantBrut());
+	        dto.setMontant_charges(c.getMontantCharges());
+	        return dto;
+	}
+	
+    public static Contrat toEntity(ContratDTO dto) {
+        if (dto == null) return null;
+
+        Contrat c = new Contrat();
+        c.setLibelle(dto.getLibelle());
+        c.setDateDebut(dto.getDate_debut());
+        c.setDateFin(dto.getDate_fin());
+        c.setMontantBrut(dto.getMontant_brut());
+        c.setMontantCharges(dto.getMontant_charges());
+        return c;
+        
+    }
+
+}

b55a51606c89e5e316bbf1dcdc5f379a88856f15
sio2 <antoninfouchet@gmail.com>
2025-12-15 09:50:25 +0100

    modif de AppartementService pour utiliser le DTO

--- src/main/java/bts/sio/azurimmo/service/AppartementService.java
+++ src/main/java/bts/sio/azurimmo/service/AppartementService.java
@@ -1,13 +1,14 @@
 package bts.sio.azurimmo.service;
 
-
 import java.util.List;
-import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import bts.sio.azurimmo.model.Appartement;
+import bts.sio.azurimmo.model.dto.AppartementDTO;
+import bts.sio.azurimmo.model.mapper.AppartementMapper;
 import bts.sio.azurimmo.repository.AppartementRepository;
 import lombok.Data;
 
@@ -19,21 +20,33 @@ public class AppartementService {
 	 private AppartementRepository appartementRepository;
 
 	 public Appartement saveAppartement(Appartement appartement) {
-		 Appartement savedAppartement = appartementRepository.save(appartement);
-	     return savedAppartement;
+		 return appartementRepository.save(appartement);
 	 }
 
-	 public List<Appartement> findByVille(String ville) {
-         return appartementRepository.findByBatiment_Ville(ville); 
+	 public List<AppartementDTO> findByVille(String ville) {
+         return appartementRepository.findByBatiment_Ville(ville)
+                                     .stream()
+                                     .map(AppartementMapper::toDTO)
+                                     .collect(Collectors.toList());
      }
 	 
-	 public List<Appartement> getAppartementsParBatiment(long id) {
-	        return appartementRepository.findByBatiment_Id(id);
+	 public List<AppartementDTO> getAppartementsParBatiment(long id) {
+        return appartementRepository.findByBatiment_Id(id)
+        							.stream()
+                                    .map(AppartementMapper::toDTO)
+                                    .collect(Collectors.toList());
      }
 	 
-	 public List<Appartement> findAppartementsBySurfaceGreaterThan(float surface) {
-	        return appartementRepository.findBySurfaceGreaterThan(surface);
-	    }
-	 
-	 
+	 public List<AppartementDTO> findAppartementsBySurfaceGreaterThan(float surface) {
+        return appartementRepository.findBySurfaceGreaterThan(surface)
+        							.stream()
+                                    .map(AppartementMapper::toDTO)
+                                    .collect(Collectors.toList());
+    }
+	
+	 public AppartementDTO saveAppartementDTO(AppartementDTO dto) {
+		 Appartement entity = AppartementMapper.toEntity(dto);
+		 Appartement saved = appartementRepository.save(entity);
+		 return AppartementMapper.toDTO(saved);
+	 }
 }
